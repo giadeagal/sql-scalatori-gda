b107cdae5a83dc3ef7a5928225ae53c0032f6a0e
Gianluca <gl.galassini@gmail.com>
2022-11-16 12:12:07 +0100

    ES5

--- esercizi.sql
+++ esercizi.sql
@@ -2,9 +2,11 @@
 1
 Calcolare l'insieme (non il multi-insieme) delle coppie (A,B) tali che A è uno scalatore e B è un continente in cui A ha effettuato una scalata.*/
 
-SELECT DISTINCT SR.cf AS scalatore,
+SELECT DISTINCT 
+    SR.cf AS scalatore,
     N.continente
-FROM scalata AS ST
+FROM 
+    scalata AS ST
     JOIN scalatore AS SR
         ON SR.cf = ST.scalatore 
     JOIN nazione AS N
@@ -13,46 +15,73 @@ FROM scalata AS ST
 /*
 2
 Per ogni scalatore nato prima del 1980, calcolare tutti i continenti in cui ha effettuato una scalata, ordinando il risultato per codice fiscale e, a parità di codice fiscale, per il nome del continente.*/
-SELECT SR.cf AS scalatore,
+SELECT 
+    SR.cf AS scalatore,
     N.nome AS continente
-FROM scalata AS ST
+FROM 
+    scalata AS ST
     JOIN scalatore AS SR
         ON SR.cf = ST.scalatore 
     JOIN nazione AS N
         ON ST.nazione = N.nome
-GROUP BY SR.cf, N.nome
-ORDER BY SR.cf
+GROUP BY
+    SR.cf, N.nome
+ORDER BY  
+    SR.cf
 
 /*
 3
 Calcolare le nazioni (mostrando, per ciascuna, anche il continente) nelle quali è stata effettuata almeno una scalata da uno scalatore minorenne.*/
 
-SELECT N.nome AS nazione,
+SELECT 
+    N.nome AS nazione,
     N.continente
-FROM scalata AS ST
+FROM  
+    scalata AS ST
     JOIN scalatore AS SR
         ON SR.cf = ST.scalatore 
     JOIN nazione AS N
         ON ST.nazione = N.nome
-WHERE (ST.anno - SR.annonascita) < 18
+WHERE 
+    (ST.anno - SR.annonascita) < 18
 
 /*
 4
 Per ogni nazione, calcolare il numero di scalate effettuate da scalatori nati in quella nazione.*/
 
-SELECT N.nome AS nazione,
+SELECT
+    N.nome AS nazione,
     count(*) AS numScalatoriNativi
-FROM scalata AS ST
+FROM 
+    scalata AS ST
     JOIN scalatore AS SR
         ON SR.cf = ST.scalatore 
     JOIN nazione AS N
         ON ST.nazione = N.nome
-WHERE ST.nazione = SR.nazionenascita
-GROUP BY N.nome
+WHERE 
+    ST.nazione = SR.nazionenascita
+GROUP BY 
+    N.nome
 
 /*
 5
 Per ogni continente, calcolare il numero di scalate effettuate da scalatori nati in una nazione di quel continente.*/
+
+SELECT 
+    N1.continente,
+    count(DISTINCT SR.cf) AS scalatenativi
+FROM 
+    scalatore AS SR
+    JOIN nazione AS N1
+        ON SR.nazionenascita = N1.nome
+    JOIN scalata AS ST
+        ON ST.scalatore = SR.cf
+    JOIN nazione AS N2
+        ON ST.nazione = N2.Nome
+WHERE
+    N1.continente = N2.continente
+GROUP BY
+    N1.continente
 
 /*
 6
